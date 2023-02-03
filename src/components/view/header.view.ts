288853cc63a9ec05b49bf6552a3e3c58af8c750a
Alimossim <agaev.alimosim@gmail.com>
2023-02-04 01:25:12 +0600

    fix: solve conflicts

--- src/components/view/header.view.ts
+++ src/components/view/header.view.ts
@@ -1,26 +1,37 @@
-import { HtmlElem } from '../../utils/HtmlElement';
+import { Control } from '../../utils/Control';
 
-class Header {
+class HeaderView {
     render(): HTMLElement {
-        const header = new HtmlElem('header', 'header');
-        const logo = new HtmlElem('a', 'header__logo');
-        logo.element.textContent = 'Task manager';
-        logo.append(header.element);
-
+        const header = new Control<HTMLElement>('header', 'header');
+        const logo = new Control<HTMLLinkElement>('a', 'header__logo');
+        const menu = new Control<HTMLElement>('nav', 'header__menu');
+        const menuList = new Control<HTMLUListElement>('ul', 'header__menu-list');
         const menuLinks = [
             ['Dashboard', 'href'],
             ['About Us', 'href'],
         ];
-        // const menu = new HtmlElem('nav','header__menu');
-        // const menuList = new HtmlElem('ul','header__menu-list');
+        const userBlock = new Control<HTMLDivElement>('div', 'header__user-block');
+        const userIcon = new Control<HTMLImageElement>('img', 'header__user-block-img');
+        const userInfo = new Control<HTMLSpanElement>('span', 'header__user-block-info');
+        userIcon.element.src = '../../assets/img/icon.svg';
+        userIcon.append(userBlock.element);
+        userInfo.element.textContent = 'Clayton Santos';
+        userInfo.append(userBlock.element);
+        logo.element.textContent = 'Task manager';
+        logo.append(header.element);
+        menu.append(header.element);
+        userBlock.append(header.element);
+        menuList.append(menu.element);
         menuLinks.forEach((item) => {
-            // const menuListItem =  new HtmlElem('li','header__menu-list-item');
-            const menuListLink = new HtmlElem('a', 'header__menu-list-link');
+            const menuListItem = new Control<HTMLLIElement>('li', 'header__menu-list-item');
+            const menuListLink = new Control<HTMLLinkElement>('a', 'header__menu-list-item-link');
             menuListLink.element.textContent = `${item[0]}`;
-            // menuListLink.element.href = `${item[0]}`;
+            menuListLink.element.href = `${item[1]}`;
+            menuListLink.append(menuListItem.element);
+            menuListItem.append(menuList.element);
         });
         return header.element;
     }
 }
 
-export default new Header();
+export default new HeaderView();
