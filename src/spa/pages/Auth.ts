bc4712e11a644cb86109afb7f14ec8c79662ba33
Viacheslav Bartosh <231globus@gmail.com>
2023-02-03 23:39:23 +0000

    refactor: refactored login, footer components to DOM

--- src/spa/pages/Auth.ts
+++ src/spa/pages/Auth.ts
@@ -1,8 +1,7 @@
-import { renderFooter } from '../../components/view/footer';
-import { renderLoginForm } from '../../components/view/login';
-import { renderSignupForm } from '../../components/view/signup';
 import { AbstractView } from '../AbstractView';
 import { QueryStringParams } from '../types';
+import loginView from '../../components/view/login.view';
+import footerView from '../../components/view/footer.view';
 
 export class Auth extends AbstractView {
     constructor(params: QueryStringParams) {
@@ -12,20 +11,14 @@ export class Auth extends AbstractView {
 
     async getHtml() {
         return `
-        <h1 class="main-title">Authentication</h1>`;
+        <h1 class="main-title visually-hidden">Authentication</h1>`;
     }
 
     async mounted() {
         const params: URLSearchParams = new URLSearchParams(document.location.search);
-        let inner = '';
-        if (params.get('type') === 'login') inner = renderLoginForm();
-        else if (params.get('type') === 'signup') inner = renderSignupForm();
-        document.body.innerHTML = `
-        <main class="auth-content">
-            <h2>Task manager</h2>
-            ${inner}
-        </main>
-        ${renderFooter()}
-        `;
+        const { body } = document;
+        if (params.get('type') === 'login') body.append(loginView.render());
+        else if (params.get('type') === 'signup') body.append(loginView.render());
+        body.append(footerView.render());
     }
 }
