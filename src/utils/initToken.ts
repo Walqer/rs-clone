0da0decf5e296228ea1cf2c40a7546b56809d36b
231globus <231globus@gmail.com>
2023-02-13 00:34:40 +0300

    feat: added validation for life cycle a token

--- src/utils/initToken.ts
+++ src/utils/initToken.ts
@@ -1,5 +1,16 @@
+import { parseJwt } from '../api/helper';
 import { state } from '../store/state';
 
 export function initToken() {
     state.token = localStorage.getItem('token');
+    if (state.token) {
+        const { exp } = parseJwt(state.token);
+        const expTime = exp * 1000;
+        const curTime = new Date().getTime();
+        if (expTime - curTime >= -3000) {
+            localStorage.removeItem('token');
+            state.token = null;
+            console.log('token is invalid');
+        }
+    }
 }
