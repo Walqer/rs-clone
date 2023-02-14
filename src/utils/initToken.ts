2d2fb6334f0314ca682707530d81f3421b86d8a4
Vladimir Kurganov <err0rfrost@gmail.com>
2023-02-14 17:23:50 +0600

    feat: add link to main page

--- src/utils/initToken.ts
+++ src/utils/initToken.ts
@@ -6,10 +6,12 @@ export function initToken() {
     if (state.token) {
         const { exp } = parseJwt(state.token);
         const expTime = exp * 1000;
-        const curTime = new Date().getTime();
-        if (expTime - curTime >= -3000) {
-            localStorage.removeItem('token');
-            state.token = null;
+        const now = new Date();
+        const offset = now.getTimezoneOffset();
+        const curTime = now.getTime() + offset * 60 * 1000;
+        if (curTime > expTime) {
+            // localStorage.removeItem('token');
+            // state.token = null;
             console.log('token is invalid');
         }
     }
