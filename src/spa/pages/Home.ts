0fc58cfb959fdf8cbab4f96ab7c94368396f96d7
Vladimir Kurganov <err0rfrost@gmail.com>
2023-02-24 00:56:35 +0600

    feat: add images
    add background images to workspace & board
    add main page image

--- src/spa/pages/Home.ts
+++ src/spa/pages/Home.ts
@@ -24,14 +24,17 @@ export class Home extends AbstractView {
         const main = new Control<HTMLElement>('main', 'home-content');
         const wrapper = new Control<HTMLElement>('div', 'home-content-wrapper');
         const greating = new Control<HTMLElement>('div', 'home-content__greating');
+        const greatingDesc = new Control<HTMLElement>('div', 'home-content__greating-desc');
         const h2 = new Control<HTMLElement>('h2', 'home-content__greating-title');
         const p = new Control<HTMLElement>('p', 'home-content__greating-text');
         main.append(body);
         wrapper.append(main.element);
         greating.append(wrapper.element);
-        h2.element.innerHTML = 'MiniTrello brings all your tasks, teammates, and tools together<br><br>';
+        greatingDesc.element.innerHTML = '<img src="../assets/img/scrum3.png">';
+        greatingDesc.append(wrapper.element);
+        h2.element.innerHTML = 'MiniTrello brings all your tasks, teammates and tools together<br><br>';
         h2.append(wrapper.element);
-        p.element.innerHTML = 'Keep everything in the same place—even if your team isnt<br><br>';
+        p.element.innerHTML = "Keep everything in the same place — even if your team isn't<br><br>";
         p.append(greating.element);
         // signup widget
         greating.element.append(signupWidgetView.render());
